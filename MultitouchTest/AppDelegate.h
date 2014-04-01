82e4187871684043d6382f23fb920ceac69c4064
chris6w <chris@6waves.com>
2014-04-01 20:21:54 +0800

    Initial Commit

---
+++ MultitouchTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MultitouchTest
+//
+//  Created by Chris Lee on 1/4/14.
+//  Copyright (c) 2014 Chris Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
